076e2204e50a77d03dbe3b7c256fd815cdcbaaf8
demoreon <demoreon@gmail.com>
2023-08-08 21:57:12 +0100

    basic auth folder

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -32,7 +32,7 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized_error(error) -> str:
-    """ Unauthorized handler
+    """ Modified this error Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
